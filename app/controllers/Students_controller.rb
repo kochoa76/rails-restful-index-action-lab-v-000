99df77b742ffded37898857aecebabcf3fe6e9de
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-30 23:20:06 +0000

    automatically backed up by learn

--- app/controllers/Students_controller.rb
+++ app/controllers/Students_controller.rb
@@ -1,8 +1,8 @@
-class StudentsController < ApplicationController 
+class StudentsController < ApplicationController
 
 
-def index 
-  @students = Student.all 
-end 
+def index
+  @students = Student.all
+end
 
 end
